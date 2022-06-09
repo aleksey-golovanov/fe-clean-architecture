653b6be7f872b6a2cea7080b08f61f0dc7ff5f30
aleksey-golovanov <alexey.golovanov@rokolabs.com>
2022-06-09 10:41:09 -0300

    Add tests

--- packages/domain/__tests__/todo.spec.ts
+++ packages/domain/__tests__/todo.spec.ts
@@ -1,5 +1,36 @@
-describe("Todo entity", () => {
-  test("Creating new todo", () => {});
+import { Todo } from "../src/entities";
+import { ValidationException } from "../src/exceptions";
 
-  test("Creating new todo with invalid date", () => {});
+describe("Todo entity", () => {
+  const now = new Date(2020, 1, 10);
+  jest.useFakeTimers().setSystemTime(now);
+
+  test("Creating new todo", () => {
+    const id = 1;
+    const date = new Date(2020, 1, 10);
+    const description = "test todo";
+    const todo = new Todo(id, date, description);
+
+    expect(todo.id).toEqual(id);
+    expect(todo.date).toEqual(date);
+    expect(todo.description).toEqual(description);
+  });
+
+  test("Creating new todo with invalid date", () => {
+    expect(() => new Todo(1, new Date(2020, 1, 9), "test todo")).toThrow(
+      ValidationException
+    );
+  });
+
+  test("Changing description", () => {
+    const id = 1;
+    const date = new Date(2020, 1, 10);
+    const description = "test todo";
+    const todo = new Todo(id, date, description);
+
+    const newDescription = "changed description";
+    todo.description = newDescription;
+
+    expect(todo.description).toEqual(newDescription);
+  });
 });
