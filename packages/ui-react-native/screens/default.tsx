ca04988d6b3e1f41431af4c629dde3ff15c197c0
aleksey-golovanov <alexey.golovanov@rokolabs.com>
2022-06-23 10:49:18 -0300

    Add mobx to react native app

--- packages/ui-react-native/screens/default.tsx
+++ packages/ui-react-native/screens/default.tsx
@@ -1,19 +1,22 @@
+import {observer} from 'mobx-react-lite';
 import React, {useEffect} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {useApp} from '../hooks';
 
-export const DefaultScreen: React.FC = () => {
+export const DefaultScreen: React.FC = observer(() => {
   const {commands, queries} = useApp();
 
+  const todos = queries.getTodos();
+
+  console.log(todos);
+
   useEffect(() => {
-    commands.loadTodos().then(() => {
-      console.log(queries.getTodos());
-    });
-  }, [commands, queries]);
+    commands.loadTodos();
+  }, [commands]);
 
   return (
     <SafeAreaView>
       <Text>DEFAULT SCREEN</Text>
     </SafeAreaView>
   );
-};
+});
